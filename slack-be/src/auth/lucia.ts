af96adbb5d5260a317e41974b327d4818a202454
Dhananjay-JSR <ajaysenday@gmail.com>
2024-05-12 02:54:59 +0530

    Setup MongoDB Connection

--- slack-be/src/auth/lucia.ts
+++ slack-be/src/auth/lucia.ts
@@ -1,1 +1,22 @@
 import { Lucia } from "lucia";
+import { MongodbAdapter } from "@lucia-auth/adapter-mongodb";
+import mongoose from "mongoose";
+const adapter = new MongodbAdapter(
+  mongoose.connection.collection("sessions"),
+  mongoose.connection.collection("users")
+);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+// IMPORTANT!
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+  }
+}
--- slack-be/src/index.ts
+++ slack-be/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Gello World");
+import DotEnv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+DotEnv.config({
+  path: ".env.local",
+});
+
+const PORT = process.env.PORT || 4000;
+
+const App = express();
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("Connected to MongoDB");
+    App.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
